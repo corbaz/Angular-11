10f3259a4fc8bdb961949fa301d1050b504328d1
corbaz <julio.corbaz@gmail.com>
2021-02-08 19:41:12 -0300

    Version:20.02.08.1941

---
+++ git-init.bat
@@ -0,0 +1,9 @@
+REM  "description": "API realizada en Deno-Servidor: OAK-Base de Datos: Mysql",
+REM  "homepage": "http://corbaz.github.io/deno-mysql",
+REM  echo "Angular 11" >> README.md
+git init
+git add .
+git commit -m "version inicial"
+git branch -M main
+git remote add origin https://github.com/corbaz/Angular-11
+git push -u origin main
---
+++ git-push.bat
@@ -0,0 +1,48 @@
+git add .
+rem @echo off
+rem set /P mensaje=Ingrese un commit: 
+
+@echo off
+set HORA_COMPLETA=%TIME%
+set HORA=%HORA_COMPLETA:~0,2%
+
+set MINUTOS=%HORA_COMPLETA:~3,2%
+set SEGUNDOS=%HORA_COMPLETA:~6,2%
+
+if %HORA% LSS 10 (
+    set HORA=0%HORA_COMPLETA:~1,1%
+)    
+@echo HORA
+@echo %HORA%
+
+@echo Minutos
+@echo %MINUTOS%
+
+@echo Segundos
+@echo %SEGUNDOS%
+
+
+set FECHA_COMPLETA=%DATE%
+set DIA=%FECHA_COMPLETA:~0,2%
+
+set MES=%FECHA_COMPLETA:~3,2%
+set ANIO=%FECHA_COMPLETA:~6,2%
+
+
+@echo Dia
+@echo %DIA%
+
+@echo Mes
+@echo %MES%
+
+@echo Anio
+@echo %Anio%
+
+set  Version=Version:%Anio%.%MES%.%DIA%.%HORA%%MINUTOS%
+@echo %Version%
+
+git commit -m "%Version%"
+
+git branch -M main
+rem git remote add origin https://github.com/corbaz/Angular-11
+git push -u origin main
