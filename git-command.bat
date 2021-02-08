85c7525b93d7a919dab59b40d2c4a5df3b141898
corbaz <julio.corbaz@gmail.com>
2021-02-08 19:31:40 -0300

    Proyecto Inicial

---
+++ git-command.bat
@@ -0,0 +1,15 @@
+REM …or create a new repository on the command line
+REM 
+REM echo "# Angular-11" >> README.md
+REM git init
+REM git add README.md
+REM git commit -m "first commit"
+REM git branch -M main
+REM git remote add origin https://github.com/corbaz/Angular-11.git
+REM git push -u origin main
+REM 
+REM …or push an existing repository from the command line
+REM 
+REM git remote add origin https://github.com/corbaz/Angular-11.git
+REM git branch -M main
+REM git push -u origin main
